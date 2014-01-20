09284779a2351c5d7034c7acf63c13120bf5432e
danielsnider <danielsnider12@gmail.com>
2014-01-19 20:08:04 -0500

    clean up

--- tutorial/__init__.py
+++ tutorial/__init__.py
@@ -3,17 +3,12 @@ from sqlalchemy import engine_from_config
 from pyramid.authentication import SessionAuthenticationPolicy
 from pyramid.authorization import ACLAuthorizationPolicy
 from pyramid.session import UnencryptedCookieSessionFactoryConfig
-
-
 from .models import (
     DBSession,
-    # Base,
     RootFactory,
     )
 
 def main(global_config, **settings): 
-    """ This function returns a Pyramid WSGI application.
-    """
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
 
@@ -39,8 +34,8 @@ def main(global_config, **settings):
 
     return config.make_wsgi_app()
 
+
 def addroutes(config):
-    # broken out of main() so it can be used by unit tests
     config.add_route('home', '/')
     config.add_route('query1', '/r/{subreddits}')
     config.add_route('query2', '/r/{subreddits}?minsize={minsize}')
@@ -48,6 +43,5 @@ def addroutes(config):
     config.add_route('register', '/register')
     config.add_route('login', '/login')
     config.add_route('logout', '/logout')
-    config.add_route('about', '/about')
     config.add_route('favorite', '/favorite')
     
